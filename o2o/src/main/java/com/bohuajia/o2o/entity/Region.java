fe77cf79b68537a6cfbe30abbfda652e96f42e4a
ProJBH <nzbohuajia@gmail.com>
2023-09-08 15:10:57 +0200

    generate Region class and table

---
+++ o2o/src/main/java/com/bohuajia/o2o/entity/Region.java
@@ -0,0 +1,51 @@
+package com.bohuajia.o2o.entity;
+
+import java.util.Date;
+
+public class Region {
+	private Integer regionId;
+	private String regionName;
+	private Integer priority;
+	private Date createTime;
+	private Date lastUpdateTime;
+
+	public Integer getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(Integer regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+}
