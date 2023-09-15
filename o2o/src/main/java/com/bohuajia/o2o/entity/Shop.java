e72f43b31e0dab19b3905f8dfedcd9e26ec7bed9
ProJBH <nzbohuajia@gmail.com>
2023-09-15 12:19:37 +0200

    Create Shop.java

---
+++ o2o/src/main/java/com/bohuajia/o2o/entity/Shop.java
@@ -0,0 +1,139 @@
+package com.bohuajia.o2o.entity;
+
+import java.util.Date;
+
+public class Shop {
+	// Primary Id
+	private Long shopId;
+	private String shopName;
+	private String shopDesc;
+	private String shopAddr;
+	// Shop contact number
+	private String phone;
+	// Shop picture
+	private String shopImg;
+	private Integer priority;
+	private Date createTime;
+	private Date lastEditTime;
+	// -1.Unable 0.Reviewing 1.Enable
+	private Integer enableStatus;
+	// Advice from root user
+	private String advice;
+	private Region region;
+	private PersonInfo owner;
+	private ShopCategory shopCategory;
+
+	public Long getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(Long shopId) {
+		this.shopId = shopId;
+	}
+
+	public String getShopName() {
+		return shopName;
+	}
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+	public String getShopDesc() {
+		return shopDesc;
+	}
+
+	public void setShopDesc(String shopDesc) {
+		this.shopDesc = shopDesc;
+	}
+
+	public String getShopAddr() {
+		return shopAddr;
+	}
+
+	public void setShopAddr(String shopAddr) {
+		this.shopAddr = shopAddr;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getShopImg() {
+		return shopImg;
+	}
+
+	public void setShopImg(String shopImg) {
+		this.shopImg = shopImg;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+
+	public String getAdvice() {
+		return advice;
+	}
+
+	public void setAdvice(String advice) {
+		this.advice = advice;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
+	public PersonInfo getOwner() {
+		return owner;
+	}
+
+	public void setOwner(PersonInfo owner) {
+		this.owner = owner;
+	}
+
+	public ShopCategory getShopCategory() {
+		return shopCategory;
+	}
+
+	public void setShopCategory(ShopCategory shopCategory) {
+		this.shopCategory = shopCategory;
+	}
+
+}
+
