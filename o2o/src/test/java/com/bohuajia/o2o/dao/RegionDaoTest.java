9f96454cf302ccbab143dcd82cb4a897e5e841ff
ProJBH <nzbohuajia@gmail.com>
2023-11-04 22:15:15 +0100

    add unit test for region dao

--- o2o/src/test/java/com/bohuajia/o2o/dao/RegionDaoTest.java
+++ o2o/src/test/java/com/bohuajia/o2o/dao/RegionDaoTest.java
@@ -1,5 +1,22 @@
 package com.bohuajia.o2o.dao;
 
-public class RegionDaoTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bohuajia.o2o.BaseTest;
+import com.bohuajia.o2o.entity.Region;
+
+public class RegionDaoTest extends BaseTest{
+	@Autowired
+	private RegionDAO regionDao;
+	
+	@Test
+	public void testQueryArea(){
+		List<Region> areaList = regionDao.queryRegion();
+		assertEquals(2, areaList.size());
+	}
 }
