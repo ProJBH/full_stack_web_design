c32c51362d2cd78a3a1bb5565625c9cb9c76ecfa
ProJBH <nzbohuajia@gmail.com>
2023-11-05 21:06:00 +0100

    implement getRegionList

--- o2o/src/main/java/com/bohuajia/o2o/service/impl/RegionServiceImpl.java
+++ o2o/src/main/java/com/bohuajia/o2o/service/impl/RegionServiceImpl.java
@@ -1,7 +1,19 @@
 package com.bohuajia.o2o.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bohuajia.o2o.dao.RegionDAO;
+import com.bohuajia.o2o.entity.Region;
 import com.bohuajia.o2o.service.RegionService;
 
 public class RegionServiceImpl implements RegionService{
+	@Autowired
+	private RegionDAO regionDao;
+	@Override
+	public List<Region> getRegionList() {
+		return regionDao.queryRegion();
+	}
 	
 }
