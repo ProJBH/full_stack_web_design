29c0cfbcbb90e077ecd7abcf9278c2c13b0c6517
ProJBH <nzbohuajia@gmail.com>
2023-11-05 21:06:42 +0100

    add service tag

--- o2o/src/main/java/com/bohuajia/o2o/service/impl/RegionServiceImpl.java
+++ o2o/src/main/java/com/bohuajia/o2o/service/impl/RegionServiceImpl.java
@@ -3,11 +3,13 @@ package com.bohuajia.o2o.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bohuajia.o2o.dao.RegionDAO;
 import com.bohuajia.o2o.entity.Region;
 import com.bohuajia.o2o.service.RegionService;
 
+@Service
 public class RegionServiceImpl implements RegionService{
 	@Autowired
 	private RegionDAO regionDao;
