4df84ddd8745b8622e2484467931c5a97f9cc31b
ProJBH <nzbohuajia@gmail.com>
2023-11-05 21:02:22 +0100

    add get region list function

--- o2o/src/main/java/com/bohuajia/o2o/service/RegionService.java
+++ o2o/src/main/java/com/bohuajia/o2o/service/RegionService.java
@@ -1,5 +1,9 @@
 package com.bohuajia.o2o.service;
 
-public interface RegionService {
+import java.util.List;
 
+import com.bohuajia.o2o.entity.Region;
+
+public interface RegionService {
+	List<Region> getRegionList();
 }
