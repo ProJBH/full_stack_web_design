3506c01d8956c0f4f446eebe4e62e2a206c81d4b
ProJBH <nzbohuajia@gmail.com>
2023-11-07 11:50:55 +0100

    add insert shop interface

---
+++ o2o/src/main/java/com/bohuajia/o2o/dao/ShopDAO.java
@@ -0,0 +1,13 @@
+package com.bohuajia.o2o.dao;
+
+import com.bohuajia.o2o.entity.Shop;
+
+public interface ShopDAO {
+	/**
+	 * Add new Shop
+	 * 
+	 * @param shop
+	 * @return
+	 */
+	int insertShop(Shop shop);
+}
