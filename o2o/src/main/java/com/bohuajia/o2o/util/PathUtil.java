af81b6fe50ef6af2a192f7eaeed3d28f8cb7e12b
ProJBH <nzbohuajia@gmail.com>
2023-11-07 17:50:56 +0100

    add getShopImagePath func

--- o2o/src/main/java/com/bohuajia/o2o/util/PathUtil.java
+++ o2o/src/main/java/com/bohuajia/o2o/util/PathUtil.java
@@ -14,4 +14,9 @@ public class PathUtil {
 		basePath = basePath.replace("/", seperator);
 		return basePath;
 	}
+	
+	public static String getShopImagePath(long shopId) {
+		String imagePath = "/upload/images/item/shop/" + shopId + "/";
+		return imagePath.replace("/", seperator);
+	}
 }
