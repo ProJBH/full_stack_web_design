a0adfadc57a7852c59e05346d7a1c8307343649f
ProJBH <nzbohuajia@gmail.com>
2023-11-07 17:46:38 +0100

    Update ImageUtil.java

--- o2o/src/main/java/com/bohuajia/o2o/util/ImageUtil.java
+++ o2o/src/main/java/com/bohuajia/o2o/util/ImageUtil.java
@@ -13,8 +13,8 @@ public class ImageUtil {
 	private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
 	
 	public static void main(String[] args) throws IOException {
-		Thumbnails.of(new File("/Users/projbh/Github_projects/full_stack_web_design/Baymax.jpeg")).size(200, 200)
+		Thumbnails.of(new File("/Users/projbh/Github_projects/full_stack_web_design/image/Baymax.jpeg")).size(200, 200)
 				.watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")), 0.8f)
-				.outputQuality(0.8f).toFile("/Users/projbh/Github_projects/full_stack_web_design/Baymax_new.jpeg");
+				.outputQuality(0.8f).toFile("/Users/projbh/Github_projects/full_stack_web_design/image/Baymax_new.jpeg");
 	}
 }
