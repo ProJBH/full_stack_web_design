116ec1c22cdc1059284dbd58d3c8fdea7345339c
ProJBH <nzbohuajia@gmail.com>
2023-11-07 10:50:03 +0100

    add log to region controller

--- o2o/src/main/java/com/bohuajia/o2o/web/superAdmin/RegionController.java
+++ o2o/src/main/java/com/bohuajia/o2o/web/superAdmin/RegionController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,16 @@ import com.bohuajia.o2o.service.RegionService;
 
 @Controller
 @RequestMapping("/superadmin")
-public class RgionController {
+public class RegionController {
+	Logger logger = LoggerFactory.getLogger(RegionController.class);
 	@Autowired
 	private RegionService regionService;
 	@RequestMapping(value= "/listregion", method=RequestMethod.GET)
 	//@ResponseBody: transfer modelMap obj to json form to frontend
 	@ResponseBody
 	private Map<String, Object> listRegion(){
+		logger.info("===Start===");
+		long startTime = System.currentTimeMillis();
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		List<Region> list = new ArrayList<Region>();
 		try {
@@ -34,6 +39,10 @@ public class RgionController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime - startTime);
+		logger.info("===End===");
 		return modelMap;
 	}
 }
