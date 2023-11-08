03a8c7ca28afa182f24b45c635fcbe1364dbbadb
ProJBH <nzbohuajia@gmail.com>
2023-11-08 18:02:11 +0100

    impl registershop func

---
+++ o2o/src/main/java/com/bohuajia/o2o/web/shopAdmin/ShopManagementController.java
@@ -0,0 +1,98 @@
+package com.bohuajia.o2o.web.shopAdmin;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import com.bohuajia.o2o.dto.ImageHolder;
+import com.bohuajia.o2o.dto.ShopExecution;
+import com.bohuajia.o2o.entity.PersonInfo;
+import com.bohuajia.o2o.entity.Shop;
+import com.bohuajia.o2o.enums.ShopStateEnum;
+import com.bohuajia.o2o.exceptions.ShopOperationException;
+import com.bohuajia.o2o.service.ShopService;
+import com.bohuajia.o2o.util.HttpServletRequestUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Controller
+@RequestMapping("/shopadmin")
+public class ShopManagementController {
+	@Autowired
+	private ShopService shopService;
+	
+	@RequestMapping(value = "/registershop", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> registerShop(HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		// 1.Receive and convert corresponding parameters, including shop info and image info
+		String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
+		ObjectMapper mapper = new ObjectMapper();
+		Shop shop = null;
+		try {
+			shop = mapper.readValue(shopStr, Shop.class);
+		} catch (Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.getMessage());
+			return modelMap;
+		}
+		CommonsMultipartFile shopImg = null;
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		if (commonsMultipartResolver.isMultipart(request)) {
+			MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+			shopImg = (CommonsMultipartFile) multipartHttpServletRequest.getFile("shopImg");
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "Upload image cannot be null");
+			return modelMap;
+		}
+		// 2.Register shop
+		if (shop != null && shopImg != null) {
+			PersonInfo owner = (PersonInfo) request.getSession().getAttribute("user");
+			shop.setOwner(owner);
+			ShopExecution se;
+			try {
+				ImageHolder imageHolder = new ImageHolder(shopImg.getOriginalFilename(), shopImg.getInputStream());
+				se = shopService.addShop(shop, imageHolder);
+				if (se.getState() == ShopStateEnum.CHECK.getState()) {
+					modelMap.put("success", true);
+					// List of shops that this user can operate
+					@SuppressWarnings("unchecked")
+					List<Shop> shopList = (List<Shop>) request.getSession().getAttribute("shopList");
+					if (shopList == null || shopList.size() == 0) {
+						shopList = new ArrayList<Shop>();
+					}
+					shopList.add(se.getShop());
+					request.getSession().setAttribute("shopList", shopList);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", se.getStateInfo());
+				}
+			} catch (ShopOperationException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.getMessage());
+			} catch (IOException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.getMessage());
+			}
+			return modelMap;
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "Please enter shop information");
+			return modelMap;
+		}
+	}
+}
