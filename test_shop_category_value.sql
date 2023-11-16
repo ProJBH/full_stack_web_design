a475cd6aa22cb70d184e98fdf73db3265d93deb1
ProJBH <nzbohuajia@gmail.com>
2023-11-16 21:54:32 +0100

    add test values

---
+++ test_shop_category_value.sql
@@ -0,0 +1,18 @@
+INSERT INTO `tb_shop_category` VALUES 
+(10,'Second-hand Market','Second-hand Goods Trading Management','/upload/images/item/shopcategory/2017061223272255687.png',101,'2023-10-04 20:10:58','2023-10-19 08:22:00',NULL),
+(11,'Beauty and Hairdressing','Beauty and Hairdressing','/upload/images/item/shopcategory/2017061223273314635.png',99,'2023-10-04 20:12:57','2023-10-12 23:27:33',NULL),
+(12,'Food and Beverages','Food and Beverages','/upload/images/item/shopcategory/2017061223274213433.png',98,'2023-10-04 20:15:21','2023-10-12 23:27:42',NULL),
+(13,'Leisure and Entertainment','Leisure and Entertainment','/upload/images/item/shopcategory/2017061223275121460.png',97,'2023-10-04 20:19:29','2023-10-12 23:27:51',NULL),
+(14,'Used Cars','Used Cars','/upload/images/item/shopcategory/2017060420315183203.png',80,'2023-10-04 20:31:51','2023-10-04 20:31:51',10),
+(15,'Second-hand Books','Second-hand Books','/upload/images/item/shopcategory/2017060420322333745.png',79,'2023-10-04 20:32:23','2023-10-04 20:32:23',10),
+(17,'Nursing','Nursing','/upload/images/item/shopcategory/2017060420372391702.png',76,'2023-10-04 20:37:23','2023-10-04 20:37:23',11),
+(18,'Haircut','Haircut','/upload/images/item/shopcategory/2017060420374775350.png',74,'2023-10-04 20:37:47','2023-10-04 20:37:47',11),
+(20,'Street Stall','Street Stall','/upload/images/item/shopcategory/2017060420460491494.png',59,'2023-10-04 20:46:04','2023-10-04 20:46:04',12),
+(22,'Milk Tea Shop','Milk Tea Shop','/upload/images/item/shopcategory/2017060420464594520.png',58,'2023-10-04 20:46:45','2023-10-04 20:46:45',12),
+(24,'Escape Room','Escape Room','/upload/images/item/shopcategory/2017060420500783376.png',56,'2023-10-04 20:50:07','2023-10-04 21:45:53',13),
+(25,'KTV','KTV','/upload/images/item/shopcategory/2017060420505834244.png',57,'2023-10-04 20:50:58','2023-10-04 20:51:14',13),
+(27,'Training and Education','Training and Education','/upload/images/item/shopcategory/2017061223280082147.png',96,'2023-10-04 21:51:36','2023-10-12 23:28:00',NULL),
+(28,'Rental Market','Rental Market','/upload/images/item/shopcategory/2017061223281361578.png',95,'2023-10-04 21:53:52','2023-10-12 23:28:13',NULL),
+(29,'Programming Design','Programming Design','/upload/images/item/shopcategory/2017060421593496807.png',50,'2023-10-04 21:59:34','2023-10-04 21:59:34',27),
+(30,'Vocal and Dance','Vocal and Dance','/upload/images/item/shopcategory/2017060421595843693.png',49,'2023-10-04 21:59:58','2023-10-04 21:59:58',27),
+(31,'Performance Props','Performance Props','/upload/images/item/shopcategory/2017060422114076152.png',45,'2023-10-04 22:11:40','2023-10-04 22:11:40',28);
---
+++ test_shop_value.sql
@@ -0,0 +1,8 @@
+INSERT INTO `tb_shop` VALUES 
+(1,1,3,14,'Official Shop Name','Test Description','Official Address','13810524086','/upload/item/shop/1/2017091621545314507.jpg',10,'2023-10-03 00:08:32','2023-10-19 10:02:35',0,'"Under Review"'),
+(28,1,2,22,'Minion-themed Milk Tea Shop','No reservations accepted, please come directly to the store for consumption...','Located at East Garden No. 2','13810524086','/upload/images/item/shop/28/2017092601041469991.png',50,'2023-10-26 01:04:13','2023-10-19 07:52:25',1,NULL),
+(29,1,3,22,'Baoman Milk Tea Shop','Come and drink, you will know, you are my milk tea','West Garden No. 1','1211334565','/upload/images/item/shop/29/2017092601054939287.jpg',40,'2023-10-26 01:05:49','2023-10-26 01:05:49',1,NULL),
+(30,1,2,20,'Biao Brother Street Stall','Dare to say it is not delicious?','East Garden No. 1','13628763625','/upload/images/item/shop/30/2017092601063878278.jpg',30,'2023-10-26 01:06:37','2023-10-26 01:06:37',1,NULL),
+(31,1,2,20,'Wei Brother Street Stall','To defeat Biao Brother Street Stall','South Road of East Garden','126554437261','/upload/images/item/shop/31/2017092601072177572.jpg',21,'2023-10-26 01:07:21','2023-10-19 10:03:05',1,NULL),
+(32,1,2,22,'You Are My Milk Tea','The milk tea shop strikes again','Sixth Road of East Garden','13652384615','/upload/images/item/shop/32/2017092601081463136.jpg',10,'2023-10-26 01:08:13','2023-10-26 01:08:13',1,NULL),
+(35,8,2,22,'Milk Tea Is Here','Milk Tea Is Here','West Garden No. 7 Road',NULL,NULL,0,NULL,NULL,0,NULL);
