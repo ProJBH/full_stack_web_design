b80b22fd79f95ac11f636ca5608b04da0fef5b99
ProJBH <nzbohuajia@gmail.com>
2023-11-16 23:35:41 +0100

    add getShopManagementInfo func

--- o2o/src/main/java/com/bohuajia/o2o/web/shopAdmin/ShopManagementController.java
+++ o2o/src/main/java/com/bohuajia/o2o/web/shopAdmin/ShopManagementController.java
@@ -237,4 +237,29 @@ public class ShopManagementController {
 		}
 		return modelMap;
 	}
+	
+	@RequestMapping(value = "/getshopmanagementinfo", method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> getShopManagementInfo(HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		long shopId = HttpServletRequestUtil.getLong(request, "shopId");
+		if (shopId <= 0) {
+			Object currentShopObj = request.getSession().getAttribute("currentShop");
+			if (currentShopObj == null) {
+				modelMap.put("redirect", true);
+				modelMap.put("url", "/o2o/shopadmin/shoplist");
+			} else {
+				Shop currentShop = (Shop) currentShopObj;
+				modelMap.put("redirect", false);
+				modelMap.put("shopId", currentShop.getShopId());
+			}
+		} else {
+			Shop currentShop = new Shop();
+			currentShop.setShopId(shopId);
+			request.getSession().setAttribute("currentShop", currentShop);
+            modelMap.put("shopId", shopId);
+			modelMap.put("redirect", false);
+		}
+		return modelMap;
+	}
 }
