7a536694589ce6c4d87dad4c36d2af852a5141e2
ProJBH <nzbohuajia@gmail.com>
2023-11-16 21:55:13 +0100

    add queryShopList and queryShopCount interface

--- o2o/src/main/java/com/bohuajia/o2o/dao/ShopDAO.java
+++ o2o/src/main/java/com/bohuajia/o2o/dao/ShopDAO.java
@@ -1,5 +1,9 @@
 package com.bohuajia.o2o.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.bohuajia.o2o.entity.Shop;
 
 public interface ShopDAO {
@@ -18,6 +22,7 @@ public interface ShopDAO {
 	 * @return
 	 */
 	int updateShop(Shop shop);
+	
 	/**
 	 * Search shop by shopId
 	 * 
@@ -25,4 +30,27 @@ public interface ShopDAO {
 	 * @return shop
 	 */
 	Shop queryByShopId(long shopId);
+	
+	/**
+	 * To query shops in pages, 
+	 * the conditions that can be entered are: 
+	 * shop name (fuzzy), shop status, shop category, region ID, owner
+	 * 
+	 * @param shopCondition
+	 * @param rowIndex
+	 *            From which row should start fetching data
+	 * @param pageSize
+	 *            Number of returned data
+	 * @return
+	 */
+	List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex,
+			@Param("pageSize") int pageSize);
+	
+	/**
+	 * return number of queryShopList
+	 * 
+	 * @param shopCondition
+	 * @return
+	 */
+	int queryShopCount(@Param("shopCondition") Shop shopCondition);
 }
