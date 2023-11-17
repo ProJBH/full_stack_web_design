80d8489208adfe18fa7b262aad186559e6569932
ProJBH <nzbohuajia@gmail.com>
2023-11-17 18:21:04 +0100

    impl batchAddProductCategory func

--- o2o/src/main/java/com/bohuajia/o2o/service/impl/ProductCategoryServiceImpl.java
+++ o2o/src/main/java/com/bohuajia/o2o/service/impl/ProductCategoryServiceImpl.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bohuajia.o2o.dao.ProductCategoryDao;
+import com.bohuajia.o2o.dto.ProductCategoryExecution;
 import com.bohuajia.o2o.entity.ProductCategory;
+import com.bohuajia.o2o.enums.ProductCategoryStateEnum;
+import com.bohuajia.o2o.exceptions.ProductCategoryOperationException;
 import com.bohuajia.o2o.service.ProductCategoryService;
 
 @Service
@@ -17,5 +21,26 @@ public class ProductCategoryServiceImpl implements ProductCategoryService {
 	@Override
 	public List<ProductCategory> getProductCategoryList(long shopId) {
 		return productCategoryDao.queryProductCategoryList(shopId);
+	}
+
+	@Override
+	@Transactional
+	public ProductCategoryExecution batchAddProductCategory(List<ProductCategory> productCategoryList)
+			throws ProductCategoryOperationException {
+		if (productCategoryList != null && productCategoryList.size() > 0) {
+			try {
+				int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
+				if (effectedNum <= 0) {
+					throw new ProductCategoryOperationException("Product category creation failed");
+				} else {
+					return new ProductCategoryExecution(ProductCategoryStateEnum.SUCCESS);
+				}
+
+			} catch (Exception e) {
+				throw new ProductCategoryOperationException("batchAddProductCategory error: " + e.getMessage());
+			}
+		} else {
+			return new ProductCategoryExecution(ProductCategoryStateEnum.EMPTY_LIST);
+		}
 	}
 }
