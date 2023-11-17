d2212ac6ebfd04e2ae0b400979f8ec4f4b858611
ProJBH <nzbohuajia@gmail.com>
2023-11-17 17:37:53 +0100

    format code

--- o2o/src/main/webapp/resources/js/shop/productcategorymanagement.js
+++ o2o/src/main/webapp/resources/js/shop/productcategorymanagement.js
@@ -5,42 +5,42 @@ $(function() {
 	getList();
 	function getList() {
 		$
-				.getJSON(
-						listUrl,
-						function(data) {
-							if (data.success) {
-								var dataList = data.data;
-								$('.category-wrap').html('');
-								var tempHtml = '';
-								dataList
-										.map(function(item, index) {
-											tempHtml += ''
-													+ '<div class="row row-product-category now">'
-													+ '<div class="col-33 product-category-name">'
-													+ item.productCategoryName
-													+ '</div>'
-													+ '<div class="col-33">'
-													+ item.priority
-													+ '</div>'
-													+ '<div class="col-33"><a href="#" class="button delete" data-id="'
-													+ item.productCategoryId
-													+ '">删除</a></div>'
-													+ '</div>';
-										});
-								$('.category-wrap').append(tempHtml);
-							}
-						});
+			.getJSON(
+				listUrl,
+				function(data) {
+					if (data.success) {
+						var dataList = data.data;
+						$('.category-wrap').html('');
+						var tempHtml = '';
+						dataList
+							.map(function(item, index) {
+								tempHtml += ''
+									+ '<div class="row row-product-category now">'
+									+ '<div class="col-33 product-category-name">'
+									+ item.productCategoryName
+									+ '</div>'
+									+ '<div class="col-33">'
+									+ item.priority
+									+ '</div>'
+									+ '<div class="col-33"><a href="#" class="button delete" data-id="'
+									+ item.productCategoryId
+									+ '">Delete</a></div>'
+									+ '</div>';
+							});
+						$('.category-wrap').append(tempHtml);
+					}
+				});
 	}
 	$('#new')
-			.click(
-					function() {
-						var tempHtml = '<div class="row row-product-category temp">'
-								+ '<div class="col-33"><input class="category-input category" type="text" placeholder="分类名"></div>'
-								+ '<div class="col-33"><input class="category-input priority" type="number" placeholder="优先级"></div>'
-								+ '<div class="col-33"><a href="#" class="button delete">删除</a></div>'
-								+ '</div>';
-						$('.category-wrap').append(tempHtml);
-					});
+		.click(
+			function() {
+				var tempHtml = '<div class="row row-product-category temp">'
+					+ '<div class="col-33"><input class="category-input category" type="text" placeholder="Classification Name"></div>'
+					+ '<div class="col-33"><input class="category-input priority" type="number" placeholder="Priority"></div>'
+					+ '<div class="col-33"><a href="#" class="button delete">Delete</a></div>'
+					+ '</div>';
+				$('.category-wrap').append(tempHtml);
+			});
 	$('#submit').click(function() {
 		var tempArr = $('.temp');
 		var productCategoryList = [];
@@ -53,47 +53,47 @@ $(function() {
 			}
 		});
 		$.ajax({
-			url : addUrl,
-			type : 'POST',
-			data : JSON.stringify(productCategoryList),
-			contentType : 'application/json',
-			success : function(data) {
+			url: addUrl,
+			type: 'POST',
+			data: JSON.stringify(productCategoryList),
+			contentType: 'application/json',
+			success: function(data) {
 				if (data.success) {
-					$.toast('提交成功！');
+					$.toast('Success！');
 					getList();
 				} else {
-					$.toast('提交失败！');
+					$.toast('Fail！');
 				}
 			}
 		});
 	});
 
 	$('.category-wrap').on('click', '.row-product-category.temp .delete',
-			function(e) {
-				console.log($(this).parent().parent());
-				$(this).parent().parent().remove();
+		function(e) {
+			console.log($(this).parent().parent());
+			$(this).parent().parent().remove();
 
-			});
+		});
 	$('.category-wrap').on('click', '.row-product-category.now .delete',
-			function(e) {
-				var target = e.currentTarget;
-				$.confirm('确定么?', function() {
-					$.ajax({
-						url : deleteUrl,
-						type : 'POST',
-						data : {
-							productCategoryId : target.dataset.id
-						},
-						dataType : 'json',
-						success : function(data) {
-							if (data.success) {
-								$.toast('删除成功！');
-								getList();
-							} else {
-								$.toast('删除失败！');
-							}
+		function(e) {
+			var target = e.currentTarget;
+			$.confirm('Are you sure?', function() {
+				$.ajax({
+					url: deleteUrl,
+					type: 'POST',
+					data: {
+						productCategoryId: target.dataset.id
+					},
+					dataType: 'json',
+					success: function(data) {
+						if (data.success) {
+							$.toast('Successfully deleted！');
+							getList();
+						} else {
+							$.toast('Failed to delete！');
 						}
-					});
+					}
 				});
 			});
+		});
 });
