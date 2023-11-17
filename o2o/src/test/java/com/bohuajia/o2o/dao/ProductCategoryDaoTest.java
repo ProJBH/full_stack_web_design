9faef3d1ab1e622c745e6cb59919b40d77886bcb
ProJBH <nzbohuajia@gmail.com>
2023-11-17 18:19:58 +0100

    add unitest for batchInsertProductCategory

--- o2o/src/test/java/com/bohuajia/o2o/dao/ProductCategoryDaoTest.java
+++ o2o/src/test/java/com/bohuajia/o2o/dao/ProductCategoryDaoTest.java
@@ -1,7 +1,12 @@
 package com.bohuajia.o2o.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,11 +16,31 @@ import com.bohuajia.o2o.entity.ProductCategory;
 public class ProductCategoryDaoTest extends BaseTest {
 	@Autowired
 	private ProductCategoryDao productCategoryDao;
-	
+
 	@Test
+	@Ignore
 	public void testQueryByShopId() throws Exception {
 		long shopId = 28;
 		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
 		System.out.println("The number of custom categories for this store is：" + productCategoryList.size());
+	}
+
+	@Test
+	public void testABatchInsertProductCategory() {
+		ProductCategory productCategory = new ProductCategory();
+		productCategory.setProductCategoryName("Product category 1");
+		productCategory.setPriority(1);
+		productCategory.setCreateTime(new Date());
+		productCategory.setShopId(1L);
+		ProductCategory productCategory2 = new ProductCategory();
+		productCategory2.setProductCategoryName("Product category 2");
+		productCategory2.setPriority(2);
+		productCategory2.setCreateTime(new Date());
+		productCategory2.setShopId(1L);
+		List<ProductCategory> productCategoryList = new ArrayList<ProductCategory>();
+		productCategoryList.add(productCategory);
+		productCategoryList.add(productCategory2);
+		int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
+		assertEquals(2, effectedNum);
 	}
 }
