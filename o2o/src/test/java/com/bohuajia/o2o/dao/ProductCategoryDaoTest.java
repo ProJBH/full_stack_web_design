68fc1a325164abb88f6e7ad20fcb7ca4b78f7991
ProJBH <nzbohuajia@gmail.com>
2023-11-18 10:17:09 +0100

    add unitest for deletion

--- o2o/src/test/java/com/bohuajia/o2o/dao/ProductCategoryDaoTest.java
+++ o2o/src/test/java/com/bohuajia/o2o/dao/ProductCategoryDaoTest.java
@@ -6,26 +6,30 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bohuajia.o2o.BaseTest;
 import com.bohuajia.o2o.entity.ProductCategory;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductCategoryDaoTest extends BaseTest {
 	@Autowired
 	private ProductCategoryDao productCategoryDao;
 
 	@Test
 	@Ignore
-	public void testQueryByShopId() throws Exception {
+	public void testBQueryByShopId() throws Exception {
 		long shopId = 28;
 		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
 		System.out.println("The number of custom categories for this store is：" + productCategoryList.size());
 	}
 
 	@Test
+	@Ignore
 	public void testABatchInsertProductCategory() {
 		ProductCategory productCategory = new ProductCategory();
 		productCategory.setProductCategoryName("Product category 1");
@@ -42,5 +46,18 @@ public class ProductCategoryDaoTest extends BaseTest {
 		productCategoryList.add(productCategory2);
 		int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
 		assertEquals(2, effectedNum);
+	}
+
+	@Test
+	public void testCDeleteProductCategory() throws Exception {
+		long shopId = 1;
+		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
+		for (ProductCategory pc : productCategoryList) {
+			if ("Product category 1".equals(pc.getProductCategoryName())
+					|| "Product category 2".equals(pc.getProductCategoryName())) {
+				int effectedNum = productCategoryDao.deleteProductCategory(pc.getProductCategoryId(), shopId);
+				assertEquals(1, effectedNum);
+			}
+		}
 	}
 }
