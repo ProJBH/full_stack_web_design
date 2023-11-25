04b5184784b5dc0a5c9cd62e1e9e6fb28712bcec
ProJBH <nzbohuajia@gmail.com>
2023-11-25 15:23:01 +0100

    update testModifyProduct()

--- o2o/src/test/java/com/bohuajia/o2o/service/ProductServiceTest.java
+++ o2o/src/test/java/com/bohuajia/o2o/service/ProductServiceTest.java
@@ -28,6 +28,7 @@ public class ProductServiceTest extends BaseTest {
 	private ProductService productService;
 
 	@Test
+	@Ignore
 	public void testAddProduct() throws ShopOperationException, FileNotFoundException {
 		// Create a product instance with shopId 1 and productCategoryId 1 and assign values to its member variables
 		Product product = new Product();
@@ -61,7 +62,6 @@ public class ProductServiceTest extends BaseTest {
 	}
 
 	@Test
-	@Ignore
 	public void testModifyProduct() throws ShopOperationException, FileNotFoundException {
 		// Create a product instance with shopId 1 and productCategoryId 1 and assign values to its member variables
 		Product product = new Product();
@@ -69,19 +69,19 @@ public class ProductServiceTest extends BaseTest {
 		shop.setShopId(1L);
 		ProductCategory pc = new ProductCategory();
 		pc.setProductCategoryId(1L);
-		product.setProductId(1L);
+		product.setProductId(16L);
 		product.setShop(shop);
 		product.setProductCategory(pc);
 		product.setProductName("formal product");
 		product.setProductDesc("formal product");
 		// Create a thumbnail file stream
-		File thumbnailFile = new File("/Users/baidu/work/image/ercode.jpg");
+		File thumbnailFile = new File("/Users/projbh/Github_projects/full_stack_web_design/image/minions.jpg");
 		InputStream is = new FileInputStream(thumbnailFile);
 		ImageHolder thumbnail = new ImageHolder(thumbnailFile.getName(), is);
 		// Create two product detail image file streams and add them to the detail image list
-		File productImg1 = new File("/Users/baidu/work/image/xiaohuangren.jpg");
+		File productImg1 = new File("/Users/projbh/Github_projects/full_stack_web_design/image/minions.jpg");
 		InputStream is1 = new FileInputStream(productImg1);
-		File productImg2 = new File("/Users/baidu/work/image/dabai.jpg");
+		File productImg2 = new File("/Users/projbh/Github_projects/full_stack_web_design/image/Baymax.jpeg");
 		InputStream is2 = new FileInputStream(productImg2);
 		List<ImageHolder> productImgList = new ArrayList<ImageHolder>();
 		productImgList.add(new ImageHolder(productImg1.getName(), is1));
