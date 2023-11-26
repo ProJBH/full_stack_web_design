805335efec1ed61d37858e57fc61de0a32e365fb
ProJBH <nzbohuajia@gmail.com>
2023-11-26 16:49:49 +0100

    update commons

--- o2o/src/main/webapp/resources/js/frontend/index.js
+++ o2o/src/main/webapp/resources/js/frontend/index.js
@@ -1,13 +1,13 @@
 $(function() {
-	// 定义访问后台，获取头条列表以及一级类别列表的URL
+	// Define the URL to access the backend and obtain the headline list and first-level category list
 	var url = '/o2o/frontend/listmainpageinfo';
-	// 访问后台，获取头条列表以及一级类别列表
+	// Visit the backend to get the headline list and first-level category list
 	$.getJSON(url, function(data) {
 		if (data.success) {
-			// 获取后台传递过来的头条列表
+			// Get the list of headlines passed from the background
 			var headLineList = data.headLineList;
 			var swiperHtml = '';
-			// 遍历头条列表，并拼接出轮播图组
+			// Traverse the headline list and splice the carousel image group
 			headLineList.map(function(item, index) {
 				swiperHtml += '' + '<div class="swiper-slide img-wrap">'
 						+ '<a href="' + item.lineLink
@@ -15,18 +15,18 @@ $(function() {
 						+ getContextPath() + item.lineImg + '" alt="'
 						+ item.lineName + '"></a>' + '</div>';
 			});
-			// 将轮播图组赋值给前端HTML控件
+			// Assign the carousel group to the front-end HTML control
 			$('.swiper-wrapper').html(swiperHtml);
-			// 设定轮播图轮换时间为3秒
+			// Set the carousel image rotation time to 3 seconds
 			$(".swiper-container").swiper({
 				autoplay : 3000,
-				// 用户对轮播图进行操作时，是否自动停止autoplay
+				// Whether to automatically stop autoplay when the user operates on the carousel image
 				autoplayDisableOnInteraction : false
 			});
-			// 获取后台传递过来的大类列表
+			// Get the list of major categories passed from the background
 			var shopCategoryList = data.shopCategoryList;
 			var categoryHtml = '';
-			// 遍历大类列表，拼接出俩俩(col-50)一行的类别
+			// Traverse the list of major categories and splice out two (col-50) categories in one row
 			shopCategoryList.map(function(item, index) {
 				categoryHtml += ''
 						+ '<div class="col-50 shop-classify" data-category='
@@ -38,12 +38,12 @@ $(function() {
 						+ '<img class="shop-img" src="' + getContextPath()
 						+ item.shopCategoryImg + '">' + '</div>' + '</div>';
 			});
-			// 将拼接好的类别赋值给前端HTML控件进行展示
+			// Assign the spliced categories to the front-end HTML control for display
 			$('.row').html(categoryHtml);
 		}
 	});
 
-	// 若点击"我的"，则显示侧栏
+	// If you click "My", the sidebar will be displayed
 	$('#me').click(function() {
 		$.openPanel('#panel-right-demo');
 	});
