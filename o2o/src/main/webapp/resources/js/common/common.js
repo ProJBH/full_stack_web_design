4b82cf15b58e4e6cc62fee2c2983b9b85e78e3ad
ProJBH <nzbohuajia@gmail.com>
2023-11-30 10:59:17 +0100

    Update common.js
    update commons

--- o2o/src/main/webapp/resources/js/common/common.js
+++ o2o/src/main/webapp/resources/js/common/common.js
@@ -1,21 +1,20 @@
 Date.prototype.Format = function(fmt) {
 	var o = {
-		"M+" : this.getMonth() + 1, // 月份
-		"d+" : this.getDate(), // 日
-		"h+" : this.getHours(), // 小时
-		"m+" : this.getMinutes(), // 分
-		"s+" : this.getSeconds(), // 秒
-		"q+" : Math.floor((this.getMonth() + 3) / 3), // 季度
-		"S" : this.getMilliseconds()
-	// 毫秒
+		"M+": this.getMonth() + 1, // Month
+		"d+": this.getDate(), // Day
+		"h+": this.getHours(), // Hour
+		"m+": this.getMinutes(), // Minute
+		"s+": this.getSeconds(), // Second
+		"q+": Math.floor((this.getMonth() + 3) / 3), // Season
+		"S": this.getMilliseconds() // Millisecond
 	};
 	if (/(y+)/.test(fmt))
 		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
-				.substr(4 - RegExp.$1.length));
-	for ( var k in o)
+			.substr(4 - RegExp.$1.length));
+	for (var k in o)
 		if (new RegExp("(" + k + ")").test(fmt))
 			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k])
-					: (("00" + o[k]).substr(("" + o[k]).length)));
+				: (("00" + o[k]).substr(("" + o[k]).length)));
 	return fmt;
 }
 /**
@@ -35,9 +34,9 @@ function getQueryString(name) {
 }
 
 /**
- * 获取项目的ContextPath以便修正图片路由让其正常显示
+ * Get the ContextPath of the project in order to correct the image routing so that it can be displayed normally
  * @returns
  */
-function getContextPath(){
+function getContextPath() {
 	return "/o2o/";
 }
